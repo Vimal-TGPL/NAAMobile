3dd5b6a2fcd39826fdaea569e4e84fa4c5196667
Vimal Kumar <vimal.s@technogradient.com>
2019-11-04 19:44:23 +0530

    bn-ng-idle install

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -5,12 +5,15 @@ import { map } from 'rxjs/operators';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { User } from '../models/user';
 import { environment } from '../../environments/environment';
-//import { BnNgIdleService } from 'bn-ng-idle';
+import { BnNgIdleService } from 'bn-ng-idle';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
-
-  constructor() { }
+  private currentUserSubject: BehaviorSubject<User>;
+  public currentUser: Observable<User>;
+  private api_url = environment.api_url;
+  isTimerStarted: boolean = false;
+  constructor(private http: HttpClient, private bnIdle: BnNgIdleService, private router: Router) { }
 }
