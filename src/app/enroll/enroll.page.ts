e143e75d76831212cdb2101a3cadcf669dc6ac02
technogradient <vimal.s@technogradient.com>
2019-11-05 21:27:35 +0530

    signup

--- src/app/enroll/enroll.page.ts
+++ src/app/enroll/enroll.page.ts
@@ -3,6 +3,9 @@ import { User } from '../models/user'
 import { UserService} from '../services/user.service';
 import { AuthenticationService } from '../services/authentication.service';
 import {Validators, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { first } from 'rxjs/operators';
+import { AlertController } from '@ionic/angular';
+import { HttpClient, HttpHeaders, HttpRequest} from '@angular/common/http';
 
 @Component({
   selector: 'app-enroll',
@@ -13,13 +16,13 @@ export class EnrollPage implements OnInit {
   check:boolean = false;
   registerForm: FormGroup;
 
-  constructor() { }
+  constructor(public userService: UserService,public alertctrl : AlertController, public http: HttpClient, public headers : HttpHeaders) { }
 
   ngOnInit() {
    // const Regex_email = "^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$";
     this.registerForm = new FormGroup({
-      FirstName: new FormControl('', [Validators.required, Validators.maxLength(50), Validators.pattern('^[a-zA-Z\-\']+')]),
-      LastName: new FormControl('', [Validators.required, Validators.maxLength(50), Validators.pattern('^[a-zA-Z\-\']+')]),
+      FirstName: new FormControl('', [Validators.required, Validators.maxLength(50), Validators.minLength(2), Validators.pattern('^[a-zA-Z ]+')]),
+      LastName: new FormControl('', [Validators.required, Validators.maxLength(50), Validators.pattern('^[a-zA-Z ]+')]),
       //Email: new FormControl('', [Validators.required, Validators.maxLength(100), Validators.pattern(Regex_email)]),
       Email: new FormControl('', [Validators.required, Validators.maxLength(100), Validators.email]),
       Password: new FormControl('', [Validators.required, Validators.maxLength(50), Validators.minLength(6)]),
@@ -40,18 +43,71 @@ this.check = false;
 
 postRegister(){
   if(this.registerForm.valid){
-    let formDetails: User = {
-      FirstName: this.registerForm.controls["FirstName"].value,
-      LastName: this.registerForm.controls["LastName"].value,
-      Username: this.registerForm.controls["Email"].value,
-      Password: this.registerForm.controls["Password"].value,
-      InvestorType: this.registerForm.controls["selectFormControl"].value,
-      Email: this.registerForm.controls["Email"].value,
-      IsPolicyAccepted: this.registerForm.controls["Checkbox"].value==true?'Y':'N',
-      Token:""
+    // let formDetails: User = {
+    //   FirstName: this.registerForm.controls["FirstName"].value,
+    //   LastName: this.registerForm.controls["LastName"].value,
+    //   Username: this.registerForm.controls["Email"].value,
+    //   Password: this.registerForm.controls["Password"].value,
+    //   InvestorType: this.registerForm.controls["selectFormControl"].value,
+    //   Email: this.registerForm.controls["Email"].value,
+    //   IsPolicyAccepted: this.registerForm.controls["Checkbox"].value==true?'Y':'N',
+    //   Token:""
+    // }
+
+    let postData = {
+      "firstName": this.registerForm.controls["FirstName"].value,
+      "lastName": this.registerForm.controls["LastName"].value,
+      "username": this.registerForm.controls["Email"].value,
+      "password": this.registerForm.controls["Password"].value,
+      "investorType": this.registerForm.controls["selectFormControl"].value,
+      "email": this.registerForm.controls["Email"].value,
+      "isPolicyAccepted": this.registerForm.controls["Checkbox"].value==true?'Y':'N',
+      // "isEmailVerified": ""
     }
 
-    console.log(formDetails);
+    // console.log(formDetails);
+    // this.userService.register(formDetails)
+    // .pipe(first())
+    // .subscribe(
+    //    data =>{
+    //     console.log(data);
+    //     // const alert= await this.alertctrl.create({
+    //     //   message:'An Email has been sent to your email address',
+    //     //   buttons: ['OK']
+    //     // });
+    //     // await alert.present();
+    //     this.registerForm.reset();
+    //   },
+    //    error => {
+    //     console.log(error);
+    //     // const alert= await this.alertctrl.create({
+    //     //   message:error,
+    //     //   buttons: ['OK']
+    //     // });
+    //     // await alert.present();
+    //   }
+    // )
+    
+    console.log(postData);
+    var httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    this.http.post("https://demo-api.newagealpha.com/api/Users/register",postData,httpOptions)
+    .subscribe(data=>{
+      console.log(data);
+    },error =>{
+      console.log(error);
+    })
   }
+}
+
+// hasError = (controlName: string, errorName:string)=>{
+//   return this.registerForm.controls[controlName].hasError(errorName);
+// }
+
+async presentAlert(){
+
 }
 }
